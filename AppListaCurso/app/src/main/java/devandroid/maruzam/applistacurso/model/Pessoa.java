c181f346149739386b9e7e124e61d6c9dc6d1a3d
MaruzamB <maruzambueno@gmail.com>
2023-10-25 10:01:56 -0300

    Criação Arquitetura MVC Criação da classe Pessoa

---
+++ AppListaCurso/app/src/main/java/devandroid/maruzam/applistacurso/model/Pessoa.java
@@ -0,0 +1,9 @@
+package devandroid.maruzam.applistacurso.model;
+
+public class Pessoa {
+
+    public Pessoa(){}
+
+
+
+}
--- AppListaCurso/app/src/main/java/devandroid/maruzam/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maruzam/applistacurso/view/MainActivity.java
@@ -1,14 +1,22 @@
-package devandroid.maruzam.applistacurso;
+package devandroid.maruzam.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import devandroid.maruzam.applistacurso.R;
+import devandroid.maruzam.applistacurso.model.Pessoa;
+
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
     }
 }
