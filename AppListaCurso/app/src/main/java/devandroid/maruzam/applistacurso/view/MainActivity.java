95d164231f6ba60d2e94b20178d6f4a55696ce04
MaruzamB <maruzambueno@gmail.com>
2023-10-27 10:45:32 -0300

    Implantado texts e botoes funcionalidades

--- AppListaCurso/app/src/main/java/devandroid/maruzam/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maruzam/applistacurso/view/MainActivity.java
@@ -3,6 +3,11 @@ package devandroid.maruzam.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.maruzam.applistacurso.R;
 import devandroid.maruzam.applistacurso.model.Pessoa;
@@ -14,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     String dadosPessoa;
     String dadosOutraPessoa;
+
+    EditText editPrimeiroNome;
+    EditText editSobrenome;
+    EditText editCursoDesejado;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
 
     @Override
@@ -30,12 +44,62 @@ public class MainActivity extends AppCompatActivity {
 
         outraPessoa = new Pessoa();
 
-        outraPessoa.setPrimeiroNome("Batman");
-        outraPessoa.setSobreNome("Robin");
-        outraPessoa.setCursoDesejado("Heroi");
-        outraPessoa.setTelefoneContato("88-888888888");
+        //outraPessoa.setPrimeiroNome("Batman");
+        //outraPessoa.setSobreNome("Robin");
+        //outraPessoa.setCursoDesejado("Heroi");
+        //outraPessoa.setTelefoneContato("88-888888888");
 
-        dadosPessoa = "Primeiro Nome: ";
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editCursoDesejado = findViewById(R.id.editCursoDesejado);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobrenome.setText(pessoa.getSobreNome());
+        editCursoDesejado.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               editPrimeiroNome.setText("");
+               editSobrenome.setText("");
+               editCursoDesejado.setText("");
+               editTelefoneContato.setText("");
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Aplicativo Encerrado", Toast.LENGTH_LONG).show();
+                finish();
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            outraPessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+            outraPessoa.setSobreNome(editSobrenome.getText().toString());
+            outraPessoa.setCursoDesejado(editCursoDesejado.getText().toString());
+            outraPessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+            Toast.makeText(MainActivity.this, "SALVO" +outraPessoa.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+       /* dadosPessoa = "Primeiro Nome: ";
         dadosPessoa += pessoa.getPrimeiroNome();
         dadosPessoa += "Sobrenome: ";
         dadosPessoa += pessoa.getSobreNome();
@@ -52,10 +116,12 @@ public class MainActivity extends AppCompatActivity {
         dadosOutraPessoa += outraPessoa.getCursoDesejado();
         dadosOutraPessoa += "Telefone Contato: ";
         dadosOutraPessoa += outraPessoa.getTelefoneContato();
+*/
+        Log.i("POOAndroid", pessoa.toString());
+        Log.i("POOAndroid", outraPessoa.toString());
 
 
-        int parada = 0;
-
+        
 
 
 
